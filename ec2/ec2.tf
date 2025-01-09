5f7e706892779a35c0af99ef5d842ac55ab99c5d
vrapracash <veeraprakash.bg@gmail.com>
2025-01-09 11:32:26 +0530

    script-update

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,12 +1,12 @@
-resource "aws_instance" "terraform" {
+# resource "aws_instance" "terraform" {
 
-    ami = "ami-09c813fb71547fc4f"
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
-    tags = {
-        Name = "terraform"
-    }
-}
+#     ami = "ami-09c813fb71547fc4f"
+#     instance_type = "t3.micro"
+#     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+#     tags = {
+#         Name = "terraform"
+#     }
+# }
 
 resource "aws_security_group" "allow_ssh_terraform" {
     name        = "allow_sshh" #allow_ssh is already there in my account
